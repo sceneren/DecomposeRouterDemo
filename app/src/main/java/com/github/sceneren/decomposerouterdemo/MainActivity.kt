f06aaed1f052628af1a2e7bc69d8212f4ac8ddb5
scene <376124750@qq.com>
2025-08-13 10:45:41 +0800

    优化动画

--- app/src/main/java/com/github/sceneren/decomposerouterdemo/MainActivity.kt
+++ app/src/main/java/com/github/sceneren/decomposerouterdemo/MainActivity.kt
@@ -7,20 +7,26 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 import androidx.core.view.WindowCompat
 import com.arkivanov.decompose.DecomposeExperimentFlags
 import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.stack.animation.Direction
+import com.arkivanov.decompose.extensions.compose.stack.animation.StackAnimator
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.predictiveBackAnimation
 import com.arkivanov.decompose.extensions.compose.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
-import com.arkivanov.decompose.router.stack.pushNew
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimator
+import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.replaceAll
 import com.github.sceneren.common.route.LocalStackRouter
 import com.github.sceneren.common.route.MainStackScreens
-import com.github.sceneren.common.route.NeedLogin
 import com.github.sceneren.decomposerouterdemo.ui.theme.DecomposeRouterDemoTheme
 import com.github.sceneren.featurea.camera.CameraScreen
 import com.github.sceneren.featurea.camposer.CamposerScreen
@@ -37,7 +43,7 @@ import io.github.xxfast.decompose.router.stack.rememberRouter
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalDecomposeApi::class, ExperimentalSharedTransitionApi::class)
+    @OptIn(ExperimentalSharedTransitionApi::class, ExperimentalDecomposeApi::class)
     @androidx.annotation.RequiresPermission(android.Manifest.permission.RECORD_AUDIO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +53,6 @@ class MainActivity : ComponentActivity() {
         val rootRouterContext: RouterContext = defaultRouterContext()
         setContent {
             Surface {
-
 
 
                 DecomposeExperimentFlags.duplicateConfigurationsEnabled = true
@@ -62,23 +67,23 @@ class MainActivity : ComponentActivity() {
                     ) {
                         DecomposeRouterDemoTheme {
                             SharedTransitionLayout {
-//                                ChildStack(
-//                                    stack = router.stack.value,
-//                                    animation = com.arkivanov.decompose.extensions.compose.experimental.stack.animation.stackAnimation(
-//                                        animator = com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade(),
-//                                    )
-//                                ) {
-//                                    CompositionLocalProvider(LocalAnimatedVisibilityScope provides this) {
                                 RoutedContent(
                                     router = router,
-                                    animation = stackAnimation { child ->
-                                        when (child.configuration) {
-                                            MainStackScreens.Splash -> fade()
-                                            MainStackScreens.Main -> fade()
-                                            is MainStackScreens.Login -> slide(orientation = Orientation.Vertical)
-                                            else -> slide()
-                                        }
-                                    }
+                                    animation = predictiveBackAnimation(
+                                        backHandler = LocalRouterContext.current.backHandler,
+                                        fallbackAnimation = stackAnimation { child ->
+                                            when (child.configuration) {
+                                                MainStackScreens.Splash -> fade()
+                                                MainStackScreens.Main -> fade()
+                                                is MainStackScreens.Login -> {
+                                                    loginScreenAnimator()
+                                                }
+
+                                                else -> slide()
+                                            }
+                                        },
+                                        onBack = { router.pop() }
+                                    )
                                 ) @androidx.annotation.RequiresPermission(android.Manifest.permission.RECORD_AUDIO) { screen ->
                                     Log.e(
                                         "MainActivity",
@@ -113,10 +118,6 @@ class MainActivity : ComponentActivity() {
                             }
 
                         }
-
-
-//                            }
-//                        }
                     }
                 }
             }
@@ -124,3 +125,47 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+fun loginScreenAnimator(
+    animationSpec: FiniteAnimationSpec<Float> = tween()
+): StackAnimator =
+    stackAnimator(animationSpec = animationSpec) { factor, direction, content ->
+
+        val modifier = when (direction) {
+            Direction.ENTER_FRONT -> {
+                Modifier.offsetYFactor(factor = factor)
+            }
+
+            Direction.ENTER_BACK -> {
+                Modifier.offsetXFactor(factor = factor)
+            }
+
+            Direction.EXIT_FRONT -> {
+                Modifier.offsetYFactor(factor = factor)
+            }
+
+            Direction.EXIT_BACK -> {
+                Modifier.offsetXFactor(factor = factor)
+            }
+        }
+
+        content(modifier)
+    }
+
+private fun Modifier.offsetXFactor(factor: Float): Modifier =
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        layout(placeable.width, placeable.height) {
+            placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt(), y = 0)
+        }
+    }
+
+private fun Modifier.offsetYFactor(factor: Float): Modifier =
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        layout(placeable.width, placeable.height) {
+            placeable.placeRelative(x = 0, y = (placeable.height.toFloat() * factor).toInt())
+        }
+    }
